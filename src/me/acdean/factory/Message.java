4ba9f2e9de73c7e194f1ef945a62992c257bc9b4
acdean <github@koogy.clara.co.uk>
2019-12-15 12:09:00 +0000

    Messages are now textured cubes.

--- src/me/acdean/factory/Message.java
+++ src/me/acdean/factory/Message.java
@@ -4,8 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import static processing.core.PApplet.println;
+import processing.core.PApplet;
+import processing.core.PConstants;
 import processing.core.PImage;
+import processing.core.PShape;
 
 public class Message {
 
@@ -13,7 +15,7 @@ public class Message {
 
     public static final int INACTIVE = 999;
     private static final float STEPS = 100.0f;
-    private static final float DECAL_DEPTH = 2.0f;
+    private static final float DECAL_DEPTH = 10.0f;
 
     // types
     public static final int GENERIC     = 0;
@@ -22,7 +24,9 @@ public class Message {
     public static final int PROXY       = 3;
     public static final int SUBTITLE    = 4;
     public static final int VIDEO       = 5;
+    public static final int TYPES       = 6;    // count of the above
 
+    PApplet p;
     Factory factory;
     public int type;
     int state;
@@ -31,44 +35,65 @@ public class Message {
     PImage img;
     Map<String, String> properties = new HashMap<>();
     float x, y;
+    float rx, ry, dx, dy;
+    static PShape[] shapes = null;
 
     public Message(Factory factory, String routeName) {
-        println("Message Name", routeName);
+        logger.info("Message Name", routeName);
         this.factory = factory;
+        p = factory.p;
         Route routeIn = factory.routes.get(routeName);
-        println("Message Name", routeName, "RouteIn", routeIn);
+        logger.info("Message Name", routeName, "RouteIn", routeIn);
         this.route = routeIn;
         this.type(GENERIC);
+        init();
     }
     public Message(Factory factory, Route route) {
         this.factory = factory;
+        p = factory.p;
         this.route = route;
         this.type(GENERIC);
+        init();
+    }
+    final void init() {
+        rx = p.random(PConstants.TWO_PI);
+        ry = p.random(PConstants.TWO_PI);
+        dx = p.random(-.04f, .04f);
+        dy = p.random(-.04f, .04f);
+        if (shapes == null) {
+            shapes = new PShape[TYPES];
+            for (int i = 0 ; i < TYPES ; i++) {
+                shapes[i] = p.createShape(PConstants.BOX, 50);
+                shapes[i].setTextureMode(PConstants.IMAGE);
+                switch(i) {
+                    case GENERIC:
+                        img = factory.messageImg;
+                        break;
+                    case CLIP:
+                        img = factory.clipImg;
+                        break;
+                    case AUDIO:
+                        img = factory.audioImg;
+                        break;
+                    case VIDEO:
+                        img = factory.videoImg;
+                        break;
+                    case PROXY:
+                        img = factory.proxyImg;
+                        break;
+                    case SUBTITLE:
+                        img = factory.subtitleImg;
+                        break;
+                }
+                shapes[i].setTexture(img);
+                shapes[i].endShape();
+            }
+        }
     }
 
     // add a delay to a message
     public Message type(int type) {
         this.type = type;
-        switch(type) {
-            case GENERIC:
-                img = factory.messageImg;
-                break;
-            case CLIP:
-                img = factory.clipImg;
-                break;
-            case AUDIO:
-                img = factory.audioImg;
-                break;
-            case VIDEO:
-                img = factory.videoImg;
-                break;
-            case PROXY:
-                img = factory.proxyImg;
-                break;
-            case SUBTITLE:
-                img = factory.subtitleImg;
-                break;
-        }
         return this;
     }
 
@@ -106,9 +131,16 @@ public class Message {
 
     void draw() {
         if (count > 0 && count != INACTIVE) {
+            rx += dx;
+            ry += dy;
             x = route.x(count / STEPS);
             y = route.y(count / STEPS);
-            factory.p.image(img, x, y);
+            p.pushMatrix();
+            p.translate(x, y, DECAL_DEPTH);
+            p.rotateX(rx);
+            p.rotateY(ry);
+            p.shape(shapes[type]);
+            p.popMatrix();
         }
     }
 
